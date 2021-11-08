e724b13bf2628ba5db83c743d9a4655272b6377e
Vince <vince@modusinternet.com>
2021-11-07 17:36:42 -0800

    asdf

--- ccmslib/site.php
+++ ccmslib/site.php
@@ -3,5 +3,7 @@
 function load_resource($arg){
 	global $CFG;
 	//echo $CFG["RES"][$arg[0]];
-	echo $CFG["RES"][$arg];
+	if((isset($CFG["RES"][$arg]) ?? null) !== "") {
+		echo $CFG["RES"][$arg];
+	}
 }
