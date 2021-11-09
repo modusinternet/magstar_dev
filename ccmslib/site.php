ad8e8a1f1b8deba9a437077ce3e80394ec27af45
Vince <vince@modusinternet.com>
2021-11-09 09:43:02 -0800

    asdf

--- ccmslib/site.php
+++ ccmslib/site.php
@@ -9,3 +9,20 @@ function load_resource($arg){
 		echo "";
 	}
 }
+
+
+function navLngList() {
+	global $CFG, $CLEAN;
+	// this line of code produces the wrong output on GoDaddy servers.
+	$tpl = htmlspecialchars(preg_replace('/^\/([\pL\pN\-]*)\/?(.*)\z/i', '${2}', $_SERVER['REQUEST_URI']));
+	$qry = $CFG["DBH"]->prepare("SELECT * FROM `ccms_lng_charset` WHERE `status` = 1 ORDER BY lngDesc ASC;");
+	if($qry->execute()) {
+		while($row = $qry->fetch()) {
+			if($row["ptrLng"]) {
+				echo "<li><a id=\"lng-" . $row["lng"] . "\" dir=\"" . $row["dir"] . "\" href=\"/" . $row["ptrLng"] . "/" . $tpl . "\" title=\"" . $row["lngDesc"] . "\">" . $row["lngDesc"] . "</a></li>";
+			} else {
+				echo "<li><a id=\"lng-" . $row["lng"] . "\" dir=\"" . $row["dir"] . "\" href=\"/" . $row["lng"] . "/" . $tpl . "\" title=\"" . $row["lngDesc"] . "\">" . $row["lngDesc"] . "</a></li>";
+			}
+		}
+	}
+}
