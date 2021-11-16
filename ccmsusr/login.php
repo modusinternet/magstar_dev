7c820dd03ae03734c679a411079acfe443e91879
Vince <vince@modusinternet.com>
2021-11-16 12:35:27 -0800

    asdf

--- ccmsusr/login.php
+++ ccmsusr/login.php
@@ -360,7 +360,7 @@ header("aa_inside: 1");
 
 header("aa_ccms_pass_reset_message: " . $ccms_pass_reset_message["FAIL"]);
 
-	if(($ccms_pass_reset_message["FAIL"] ?? null) === "") {
+	if($ccms_pass_reset_message["FAIL"] ?? null) {
 		// This is an password reset submittion, so first we need to make sure the ccms_pass_reset_form_code record is still available.
 
 header("aa_inside: 2");
