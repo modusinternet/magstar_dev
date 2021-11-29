e032a4849ebc59a1eab0ee2f44db17da23573f97
modusinternet <vince@modusinternet.com>
2021-11-29 11:42:04 -0800

    asdf

--- ccmslib/_default.php
+++ ccmslib/_default.php
@@ -121,11 +121,8 @@ function ccms_canonical() {
 function ccms_user_admin_slider() {
 	global $CFG, $CLEAN;
 
-	//$json_a = array();
-
 	if(isset($_SESSION["USER_ID"])) {
 		$json_a = json_decode($_SESSION["PRIV"], true);
-		//$CLEAN["json_a"] = json_decode($_SESSION["PRIV"], true);
 	}
 
 	if(($json_a["priv"]["content_manager"]["r"] ?? null) == 1): ?>
@@ -179,7 +176,6 @@ function ccms_user_admin_slider() {
 		$qry = $CFG["DBH"]->prepare("SELECT * FROM `ccms_lng_charset` ORDER BY lngDesc ASC;");
 		if($qry->execute()) {
 			while($row = $qry->fetch()) {
-				//if($json_a["priv"]["content_manager"]["lng"][$row["lng"]] == 1 || $json_a["priv"]["content_manager"]["lng"][$row["lng"]] == 2) {
 				if($json_a["priv"]["content_manager"]["r"] == 1 || $json_a["priv"]["content_manager"]["lng"][$row["lng"]] == 2) {
 					if($row["ptrLng"]) {
 						echo "<li id=\"ccms_lng-" . $row["lng"] . "\"><a href=\"/" . $row["ptrLng"] . "/" . $tpl . "\" title=\"Points to lng code: " . $row["ptrLng"] . "\">" . $row["lngDesc"] . "</a></li>";
